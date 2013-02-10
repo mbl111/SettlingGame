aa06bc2f5a2149d2257c45cf97e9491220d7a693
Heldplayer <mitchel.pyl@gmail.com>
2013-02-10 23:35:12 +0100

    Add storage classes

---
+++ src/net/specialattack/settling/common/storage/NBTTagString.java
@@ -0,0 +1,32 @@
+
+package net.specialattack.settling.common.storage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class NBTTagString extends NBTTag {
+
+    public String value;
+
+    public NBTTagString(String name) {
+        this(name, "");
+    }
+
+    public NBTTagString(String name, String base) {
+        super(name);
+
+        this.value = base;
+    }
+
+    @Override
+    public void read(DataInputStream input) throws IOException {
+        this.value = input.readUTF();
+    }
+
+    @Override
+    public void write(DataOutputStream output) throws IOException {
+        output.writeUTF(value);
+    }
+
+}

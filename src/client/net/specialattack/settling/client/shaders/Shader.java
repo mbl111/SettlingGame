efbb792f6564cdebb95c9019bab41269dda1130f
heldplayer <mitchel.pyl@gmail.com>
2014-03-19 20:02:36 +0100

    Update LWJGL + shaders

--- src/client/net/specialattack/settling/client/shaders/Shader.java
+++ src/client/net/specialattack/settling/client/shaders/Shader.java
@@ -1,22 +1,40 @@
 
 package net.specialattack.settling.client.shaders;
 
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 public class Shader {
 
-    public int programId;
+    /**
+     * The program ID of the shader
+     */
+    public final int programId;
 
     public Shader(int programId) {
         this.programId = programId;
     }
 
+    /**
+     * Binds the shader
+     */
     public void bindShader() {
+        Shader.prevShader = GL11.glGetInteger(GL20.GL_CURRENT_PROGRAM);
         GL20.glUseProgram(this.programId);
     }
 
+    /**
+     * Integer used to store the current active shader in case of other active
+     * shaders
+     */
+    public static int prevShader = 0;
+
+    /**
+     * Unbinds the current shader and returns to the previous shader
+     */
     public static void unbindShader() {
-        GL20.glUseProgram(0);
+        GL20.glUseProgram(Shader.prevShader);
+        Shader.prevShader = 0;
     }
 
 }
--- src/client/net/specialattack/settling/client/shaders/ShaderLoader.java
+++ src/client/net/specialattack/settling/client/shaders/ShaderLoader.java
@@ -4,13 +4,56 @@ package net.specialattack.settling.client.shaders;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import net.specialattack.settling.common.Settling;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
+/**
+ * Class for loading GL Shaders
+ * 
+ * @author heldplayer
+ * 
+ */
 public class ShaderLoader {
 
     public static Shader createShader(String name) {
+        BufferedReader vertex;
+        try {
+            vertex = new BufferedReader(new FileReader(new File(ShaderLoader.class.getResource("/shaders/" + name + ".vert").toURI())));
+        }
+        catch (Exception e) {
+            return null;
+        }
+        BufferedReader fragment;
+        try {
+            fragment = new BufferedReader(new FileReader(new File(ShaderLoader.class.getResource("/shaders/" + name + ".vert").toURI())));
+        }
+        catch (Exception e) {
+            try {
+                vertex.close();
+            }
+            catch (IOException e2) {}
+            return null;
+        }
+        return createShader(name, vertex, fragment);
+    }
+
+    /**
+     * Creates a shader from given parameters
+     * 
+     * @param name
+     *        Name of the shader, purely used for writing error messages
+     * @param vertex
+     *        The vertex shader part
+     * @param fragment
+     *        The fragment shader part
+     * @return A shader object representing the newly created shader
+     */
+    public static Shader createShader(String name, BufferedReader vertex, BufferedReader fragment) {
         int programId = GL20.glCreateProgram();
         int vertexId = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
         int fragmentId = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
@@ -18,49 +61,48 @@ public class ShaderLoader {
         String vertexData = null;
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(ShaderLoader.class.getResource("/shaders/" + name + ".vert").toURI())));
             String line = null;
 
             StringBuilder builder = new StringBuilder();
 
-            while ((line = reader.readLine()) != null) {
+            while ((line = vertex.readLine()) != null) {
                 builder.append(line).append('\n');
             }
 
-            reader.close();
+            vertex.close();
 
             vertexData = builder.toString();
         }
         catch (Exception e) {
-            System.err.println("Failed finding vertex shader part for " + name);
+            Settling.log.log(Level.WARNING, "Failed finding vertex shader part for " + name);
         }
 
         String fragmentData = null;
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(ShaderLoader.class.getResource("/shaders/" + name + ".frag").toURI())));
             String line = null;
 
             StringBuilder builder = new StringBuilder();
 
-            while ((line = reader.readLine()) != null) {
+            while ((line = fragment.readLine()) != null) {
                 builder.append(line).append('\n');
             }
 
-            reader.close();
+            fragment.close();
 
             fragmentData = builder.toString();
         }
         catch (Exception e) {
-            System.err.println("Failed finding fragment shader part for " + name);
+            Settling.log.log(Level.WARNING, "Failed finding fragment shader part for " + name);
         }
 
         if (vertexData != null) {
             GL20.glShaderSource(vertexId, vertexData);
             GL20.glCompileShader(vertexId);
 
-            if (GL20.glGetShader(vertexId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
-                System.err.println("Failed compiling vertex shader for " + name);
+            if (GL20.glGetShaderi(vertexId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
+                Settling.log.log(Level.WARNING, "Failed compiling vertex shader for " + name);
+                Settling.log.log(Level.WARNING, GL20.glGetShaderInfoLog(vertexId, 1024));
 
                 return null;
             }
@@ -70,15 +112,16 @@ public class ShaderLoader {
             GL20.glShaderSource(fragmentId, fragmentData);
             GL20.glCompileShader(fragmentId);
 
-            if (GL20.glGetShader(fragmentId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
-                System.err.println("Failed compiling fragment shader for " + name);
+            if (GL20.glGetShaderi(fragmentId, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
+                Settling.log.log(Level.WARNING, "Failed compiling fragment shader for " + name);
+                Settling.log.log(Level.WARNING, GL20.glGetShaderInfoLog(fragmentId, 1024));
 
                 return null;
             }
         }
 
         if (vertexData == null && fragmentData == null) {
-            System.err.println("Shader did not load for both vertex and fragment for " + name);
+            Settling.log.log(Level.WARNING, "Shader did not load for both vertex and fragment for " + name);
 
             return null;
         }
@@ -91,8 +134,9 @@ public class ShaderLoader {
         }
         GL20.glLinkProgram(programId);
 
-        if (GL20.glGetProgram(programId, GL20.GL_LINK_STATUS) == GL11.GL_FALSE) {
-            System.err.println("Failed linking shader for " + name);
+        if (GL20.glGetProgrami(programId, GL20.GL_LINK_STATUS) == GL11.GL_FALSE) {
+            Settling.log.log(Level.WARNING, "Failed linking shader for " + name);
+            Settling.log.log(Level.WARNING, GL20.glGetProgramInfoLog(programId, 1024));
 
             return null;
         }
